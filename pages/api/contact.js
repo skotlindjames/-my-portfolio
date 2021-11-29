9f70cd8a4b21dddb835a6a31493ce32e026007e6
Nazariy Dumanskyy <nazariy1995@gmail.com>
2021-11-28 21:48:50 -0800

    more config

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -8,49 +8,51 @@ export default async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ msg: 'Only POST requests are allowed' });
   }
-  try {
-    const { name, email, subject, message } = JSON.parse(req.body);
-    console.log("I am in the API")
-    await notion.pages.create({
-      parent: {
-        database_id: "9d9b8dfe0ff74092b4971259845ab6e7",
-      },
-      properties: {
-        Name: {
-          title: [
-            {
-              text: {
-                content: name,
-              },
-            },
-          ],
-        },
-        Email: {
-          email,
-        },
-        Subject: {
-          rich_text: [
-            {
-              text: {
-                content: subject,
-              },
-            },
-          ],
-        },
-        Message: {
-          rich_text: [
-            {
-              text: {
-                content: message,
-              },
-            },
-          ],
-        },
-      },
-    });
-    res.status(201).json({ msg: 'Success' });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ msg: 'Failed' });
-  }
+
+  console.log("hiiii")
+  // try {
+  //   const { name, email, subject, message } = JSON.parse(req.body);
+  //   console.log("I am in the API")
+  //   await notion.pages.create({
+  //     parent: {
+  //       database_id: "9d9b8dfe0ff74092b4971259845ab6e7",
+  //     },
+  //     properties: {
+  //       Name: {
+  //         title: [
+  //           {
+  //             text: {
+  //               content: name,
+  //             },
+  //           },
+  //         ],
+  //       },
+  //       Email: {
+  //         email,
+  //       },
+  //       Subject: {
+  //         rich_text: [
+  //           {
+  //             text: {
+  //               content: subject,
+  //             },
+  //           },
+  //         ],
+  //       },
+  //       Message: {
+  //         rich_text: [
+  //           {
+  //             text: {
+  //               content: message,
+  //             },
+  //           },
+  //         ],
+  //       },
+  //     },
+  //   });
+  //   res.status(201).json({ msg: 'Success' });
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(500).json({ msg: 'Failed' });
+  // }
 };
