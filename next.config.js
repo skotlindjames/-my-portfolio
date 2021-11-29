08e63461a16a01364fdbad9b31d8e70069a8986a
Nazariy Dumanskyy <nazariy1995@gmail.com>
2021-11-28 17:02:21 -0800

    add vercel integration

--- next.config.js
+++ next.config.js
@@ -3,21 +3,7 @@ module.exports = {
     domains: [
       'res.cloudinary.com',
       'avatars.githubusercontent.com',
-      'imgur.com',
+      'imgur.com'
     ],
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "https://portfolio-mkx0pfwj3-nazariy995.vercel.app/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
   }
 };
