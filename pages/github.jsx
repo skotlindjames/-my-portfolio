9ecba88417cef95c82578d96f5ca73312a1be2aa
Scotland Yannello <85612295+skotlindjames@users.noreply.github.com>
2022-01-27 16:32:34 -0800

    Update github.jsx
    fixed the GitHub username

--- pages/github.jsx
+++ pages/github.jsx
@@ -41,7 +41,7 @@ const GithubPage = ({ repos, user }) => {
       </div>
       <div className={styles.contributions}>
         <GitHubCalendar
-          username="nazariy995"
+          username="skotlindjames"
           theme={theme}
           hideColorLegend
         />
@@ -52,12 +52,12 @@ const GithubPage = ({ repos, user }) => {
 
 export async function getStaticProps() {
   const userRes = await fetch(
-    `https://api.github.com/users/nazariy995`
+    `https://api.github.com/users/skotlindjames`
   );
   const user = await userRes.json();
 
   const repoRes = await fetch(
-    `https://api.github.com/users/nazariy995/repos?sort=created_at&per_page=10`
+    `https://api.github.com/users/skotlindjames/repos?sort=created_at&per_page=10`
   );
   const repos = await repoRes.json();
 
